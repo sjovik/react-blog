c0b15268b9aeab79eaff520476509119be9f49af
Johannes Lilja <joh.lilja@gmail.com>
2016-06-21 21:53:27 +0200

    Add spinner component

--- app/components/spinner.jsx
+++ app/components/spinner.jsx
@@ -2,10 +2,19 @@ import styles from './spinner.styl';
 
 import React from 'react';
 
-const Spinner = ({children}) => (
-  <div className={styles.spinner}>
-    {children}
-  </div>
+const Spinner = ({spinning, children}) => (
+    <div className={styles.spinner} data-spinning={spinning}>
+      {children}
+    </div>
 );
+
+Spinner.propTypes = {
+  spinning: React.Proptypes.bool.isRequired,
+  children: React.Proptypes.node
+};
+
+Spinner.defaultProps = {
+  spinning: false
+};
 
 export default Spinner;
